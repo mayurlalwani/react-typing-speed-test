6c81d3d7132ba62eb09493f64fa0cb3ec6f064fd
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-08 10:49:25 +0530

    Added Custom Hook

--- src/App.js
+++ src/App.js
@@ -1,50 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import './App.css';
+import useCustomHook from './useCustomHook'
 
 function App() {
 
-  const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(5)
-  const [gameStatus, setGameStatus] = useState(false)
-  const [wordCount, setWordCount] = useState(0)
-  const textAreaRef = useRef(null)
-
-  function handleChange(event){
-    const { value } = event.target
-    setText(value)
-
-  }
-
-  function countWord(text){
-    const words = text.trim().split(" ")
-    return words.filter(word=>word !== "").length
-  }
-
-  function startTimer(){
-    setGameStatus(true)
-    setTimeRemaining(5)
-    setText("")
-    textAreaRef.current.disabled = false
-    textAreaRef.current.focus()
-  }
-
-  useEffect(() => {
-    
-      if(gameStatus && timeRemaining > 0){
-
-        setTimeout(() => {
-          setTimeRemaining(time => time - 1)
-        }, 1000)
-
-      }
-      else if(timeRemaining === 0){
-        setGameStatus(false)
-        const numOfWords = countWord(text)
-        setWordCount(numOfWords)
-      }
-    }, [timeRemaining, gameStatus])
-
-
+  const { textAreaRef, handleChange, text, gameStatus, timeRemaining, startTimer, wordCount } = useCustomHook()
 
   return (
     <div className="App">

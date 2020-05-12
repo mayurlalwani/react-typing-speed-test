c45c22fd4927f53ca02c9a2e5a4c0fb9d7bb3990
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-12 14:37:25 +0530

    Add random quotes file to display quotes

---
+++ src/randomQuotes.js
@@ -0,0 +1,14 @@
+export const quotesArray= [
+    {
+        quote: "Life would be a great deal easier if dead things had the decency to remain dead.",
+        Author: 'Doug MacLeod'
+    },
+
+    {
+        quote: "Tried to escape, to block out the fact that I was being eaten alive by arachnids. For some reason the only thing I could replace it with was the image of being eaten by tiny clowns.",
+        Author: 'David'
+    }
+]
+
+
+export const random = array => array[Math.floor(Math.random() * array.length)]
